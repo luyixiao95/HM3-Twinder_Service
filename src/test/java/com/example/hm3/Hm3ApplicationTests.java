dd0c9e81c6182670938943591a29c0d4a476ea6a
Luyi Xiao <xiao.luy@northeastern.edu>
2023-01-31 14:42:22 -0800

    Initial commit

---
+++ src/test/java/com/example/hm3/Hm3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hm3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hm3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
